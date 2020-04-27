c9b4fba031881a012f9aa54867b3370e6fe401a8
Lexhoo <lexhoo@yahoo.fr>
2020-04-27 15:21:59 +0200

    Doctor controler

---
+++ src/test/java/com/wildcodeschool/DoctorController/DoctorControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.DoctorController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
